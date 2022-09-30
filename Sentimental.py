9048b92fb879b130bf4862b2da4a9133379d3743
RamenBoy0 <isaactanyiwei@gmail.com>
2022-09-30 16:53:54 +0800

    updates to data crawling codes

--- Sentimental.py
+++ Sentimental.py
@@ -1,36 +1,53 @@
+from turtle import title
 import matplotlib.pyplot as plt
 import seaborn as sns
 color = sns.color_palette()
-from nltk.corpus import stopwords
 from wordcloud import WordCloud, STOPWORDS
 import pandas as pd
 import plotly.express as px
+import numpy as np
+from PIL import Image
 
-df = pd.read_csv('data.csv') # Read data from csv
-
+df = pd.read_csv('data.csv')  # Read data from csv
+df['Tweet'] = df['Tweet'].str.lower()  # Convert tweets to lower caps
+print(len(df))
 
 # Creates a histogram based on the number of likes for each tweet
-fig = px.histogram(df, x="Number of Likes") #Takes data from the column "Likes"
-fig.update_traces(marker_color="red",marker_line_color='rgb(1,12,100)',
+fig = px.histogram(df, x="Emotion", text_auto = True) # Takes data from the column "Number of Likes"
+fig.update_traces(marker_color="blue", textfont_size = 20,
                   marker_line_width=1)
-fig.update_layout(title_text='Likes from tweets')
+fig.update_layout(title_text='Polarity of tweets')
 fig.show()
 
 
-# print(df[df['Number of Likes'] > 5]) # Printing tweets with more than 5 likes
+df_positive = df.loc[df['Emotion'] == "Positive"] # Selecting columns with positive emotion
+df_negative = df.loc[df['Emotion'] == "Negative"] # Selecting columns with negative emotion
 
-df.isna().sum
-df.dropna(inplace=True) # Drop any null values
-print(df.head) # Print out the headers 
-df['Tweet'] = df['Tweet'].str.lower() # Convert tweets to lower caps
+# Creating a wordcloud with different emotions
+def wordcloud(tweet, title):
+    image = np.array(Image.open('hashtag.png'))
+    stopwords = set(STOPWORDS)
+    stopwords.update(["br", "href"])
+    words = " ".join(tweets for tweets in tweet.Tweet)
+    wordcloud = WordCloud(width=1000, height=800, mask=image,
+                        background_color="white", stopwords=stopwords, min_font_size=10).generate(words)
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.savefig('wordcloud10.png')
+    plt.title(title, size = 20)
+    plt.show()
 
-# Creating a wordcloud
-stopwords = set(STOPWORDS)
-stopwords.update(["br", "href"])
-words = " ".join(tweets for tweets in df.Tweet)
-wordcloud = WordCloud(width = 500, height = 500, 
-background_color = "white", stopwords=stopwords, min_font_size=10).generate(words)
-plt.imshow(wordcloud)
-plt.axis("off")
-plt.savefig('wordcloud10.png')
-plt.show()
+wordcloud_p = wordcloud(df_positive, "Positive Word Cloud")
+wordcloud_n = wordcloud(df_negative, "Negative Word Cloud")
+
+
+# Graph to show sentiments over time
+print(len(df))
+neg = df[df['Emotion']=='negative']
+neg = neg.groupby(['Date Created'],as_index=False).count()
+
+pos = df[df['Emotion']=='positive']
+pos = pos.groupby(['Date Created'],as_index=False).count()
+
+pos = pos[['Date Created','id']]
+neg = neg[['Date Created','id']]
